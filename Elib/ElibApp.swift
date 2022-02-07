e95a2e2072a7ce9b3161d19ccfec0ffc33394e20
Dan Akmat <akmatdan@gmail.com>
2022-02-07 15:55:15 +0600

    Initial Commit

---
+++ Elib/ElibApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElibApp.swift
+//  Elib
+//
+//  Created by Daniil Akmatov on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ElibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
