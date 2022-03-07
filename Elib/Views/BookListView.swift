5a1e27ca719477952a34c6df26e4bee0257bd6fd
Dan Akmat <akmatdan@gmail.com>
2022-03-08 02:38:51 +0600

    ImageLoader from Firebase Storage

--- Elib/Views/BookListView.swift
+++ Elib/Views/BookListView.swift
@@ -13,46 +13,39 @@ struct BookListView: View {
     @ObservedObject var model = BooksViewModel()
     
     var body: some View {
-        
         NavigationView {
-            
             VStack(alignment: .leading) {
                 SearchBar(searchText: $searchText)
                 
                 List(filteredBooks, id: \.self) { books in
                     
                     NavigationLink(destination: BookDetailView(book: books)) {
-                    HStack() {
-                        Image(books.imageName)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 120)
-                            .cornerRadius(5)
+                        HStack() {
+                            ImageLoader(url: books.imageName)
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                
+                                Text(books.title)
+                                    .fontWeight(.semibold)
+                                    .lineLimit(2)
+                                    .minimumScaleFactor(0.5)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                Text(books.author)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                
+                                Text(books.isbn)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
                         
-                        VStack(alignment: .leading, spacing: 5) {
-                            
-                            Text(books.title)
-                                .fontWeight(.semibold)
-                                .lineLimit(2)
-                                .minimumScaleFactor(0.5)
-                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            
-                            Text(books.author)
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                            
-                            Text(books.isbn)
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
+                            VStack(alignment: .leading, spacing: 5) {
+                                Image(systemName: "heart")
+                                    .frame(width: 20, height: 30)
+                                Spacer()
+                            }
                         }
-                        
-                        VStack(alignment: .leading, spacing: 5) {
-                            Image(systemName: "heart")
-                                .frame(width: 20, height: 30)
-                            Spacer()
-                        }
-                    }
-
                     }
                 }
             }
